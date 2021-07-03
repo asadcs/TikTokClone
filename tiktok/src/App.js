dad9081ea46e75f6d861262de237f1bc759776a1
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-07-03 05:53:22 -0700

    scroll-snap implemented
    scroll-snap implemented

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
+import Video from "./Video";
 
 function App() {
-  return <div className="App">ASad Tik Tok</div>;
+  return (
+    <div className="app__videos">
+      ASad Tik Tok
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+    </div>
+  );
 }
 
 export default App;
