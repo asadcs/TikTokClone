690b2f0037c462392104c4cdf618b9c1692a5f6b
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-07-04 17:19:35 -0700

    Video side bar implemented

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./Video.css";
 import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 const Video = () => {
   const VideoRef = useRef(null);
   const [play, setPlay] = useState(false);
@@ -25,7 +26,12 @@ const Video = () => {
         onClick={onVideoPress}
         src="http://www.newdesignfile.com/postpic/2009/07/studio-background-psd-free-download_139168.jpg"
       />
-      <VideoFooter />
+      <VideoFooter
+        channel="asadullahabkari 786"
+        description="Check out the video"
+        song="hello how aree you ?"
+      />
+      <VideoSidebar likes={509} messages={"messages"} shares={"309"} />
     </div>
   );
 };
--- tiktok/src/VideoFooter.js
+++ tiktok/src/VideoFooter.js
@@ -3,25 +3,28 @@ import "./VideoFooter.css";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
 import Ticker from "react-ticker";
 
-const VideoFooter = () => {
+const VideoFooter = ({ channel, description, song }) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        {/* <h3>@{channel}</h3> */}
-        <h3>asadullahabkari</h3>
-        <p>Channel for his fame</p>
+        <h3>@{channel}</h3>
+        <p>{description}</p>
         <div className="videoFooter__ticker">
           <MusicNoteIcon className="videoFooter__icon" />
           <Ticker mode="smooth">
-            {({ index }) => <p>Hello how are you ?</p>}
+            {({ index }) => (
+              <>
+                <p>{song}</p>
+              </>
+            )}
           </Ticker>
         </div>
       </div>
-      <img
+      {/* <img
         className="videoFooter__record"
         src="https://static.thenounproject.com/png/934821-200.png"
         alt=""
-      />
+      /> */}
     </div>
   );
 };
